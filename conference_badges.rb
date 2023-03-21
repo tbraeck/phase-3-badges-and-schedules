b0c543fddda32f016238ccbfa1286a2575079b9f
Tate <tatebraeckel@gmail.com>
2023-03-20 21:00:15 -0600

    commit tree 4542ff5e89df1ed7dea986bf87aa6f9da4238675

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+
+
+ "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(attendees)
+attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+    end
+end
+
+def assign_rooms(attendees, room)
+    attendees.map.with_index(1) do |attendee,room| 
+        "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(attendees)
+    puts batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
+    end
+    end
